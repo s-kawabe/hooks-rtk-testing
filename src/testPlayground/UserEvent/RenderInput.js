093d0733cd5796b4d9c2382e70acf760cb10387f
s-kawabe <s.kawabe2281@gmail.com>
2022-04-16 23:36:45 +0900

    chore: add jest

---
+++ src/testPlayground/UserEvent/RenderInput.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const RenderInput = () => {
+  const [input, setInput] = useState("")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RenderInput
